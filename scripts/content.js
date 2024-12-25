9206e65a2e1e2101bfff9f227d6a44a12370a671
Aryan Dhar <dhararya@msu.edu>
2024-12-25 14:15:47 -0500

    finding search element and interacting

---
+++ scripts/content.js
@@ -0,0 +1,27 @@
+// Observe DOM changes to find the search field
+const observer = new MutationObserver(() => {
+    const inputField = document.querySelector("input[id='ms-searchux-input']");
+    if (typeof inputField !== 'undefined' && inputField !== null)  {
+      console.log("Search field found:", inputField);
+  
+      // Stop observing once the element is found
+      observer.disconnect();
+  
+      inputField.value = "interacted";
+      const inputEvent = new Event('input', { bubbles: true });
+      inputField.dispatchEvent(inputEvent);
+  
+    } else {
+      console.error("Teams search field not found.");
+    }
+  });
+  
+// Start observing the DOM
+observer.observe(document.body, {
+childList: true,
+subtree: true,
+});
+
+// Set a timeout to stop observing if the element is not found within a reasonable time
+setTimeout(() => observer.disconnect(), 10000); // 10 seconds
+  
