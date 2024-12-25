bd24342a20ae1fad090de0f84f4d8cac45077969
Aryan Dhar <dhararya@msu.edu>
2024-12-25 13:29:55 -0500

    hello world on teams.microsoft.com

--- background.js
+++ background.js
@@ -1,9 +1,9 @@
 chrome.webNavigation.onCompleted.addListener((details) => {
-    if (details.url && details.url.includes("https://chatgpt.com")) {
+    if (details.url && details.url.includes("https://teams.microsoft.com")) {
       chrome.scripting.executeScript({
         target: { tabId: details.tabId },
         func: () => alert("Hello World!")
       });
     }
-  }, { url: [{ hostContains: "chatgpt.com" }] });
+  }, { url: [{ hostContains: "teams.microsoft.com" }] });
   
