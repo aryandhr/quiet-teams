bd1308b2a0929b35ca157d1c81e925f2d20c94d8
Aryan Dhar <dhararya@msu.edu>
2025-01-02 20:24:21 -0500

    remove popup on load and rename extension

--- background.js
+++
@@ -1,9 +0,0 @@
-chrome.webNavigation.onCompleted.addListener((details) => {
-    if (details.url && details.url.includes("https://teams.microsoft.com")) {
-      chrome.scripting.executeScript({
-        target: { tabId: details.tabId },
-        func: () => alert("Hello World!")
-      });
-    }
-  }, { url: [{ hostContains: "teams.microsoft.com" }] });
-  
